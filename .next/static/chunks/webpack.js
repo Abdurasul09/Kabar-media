bf2b5cae949c4cd1c4cb9aaadcd47ebe6a893843
Abdurasul <ahmedovabdurasul19@gmail.com>
2022-01-22 18:59:32 +0600

    initial

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b5df4450d116df1"; }
+/******/ 		__webpack_require__.h = function() { return "a3e9d58b940ebbee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
